03a44d880d4b2b9e7da3ef94944168abe2b2bfb5
mislavcupic <mislav.cupic@gmail.com>
2023-01-20 19:38:18 +0100

    dz js open-weather

--- vjezbaAPi/script.js
+++ vjezbaAPi/script.js
@@ -5,6 +5,11 @@ let IconDisplay = document.querySelector('.weather-icon');
 const searchBtn = document.getElementById('search-button');
 const h1City = document.querySelector('#city');
 const divRez = document.querySelector('#rezultat');
+const divRez2 = document.querySelector('#rezultat2');
+const divRez3= document.querySelector('#rezultat3');
+const divRez4 = document.querySelector('#rezultat4');
+const divRez5 = document.querySelector('#rezultat5');
+const divRez6 = document.querySelector('#rezultat6');
 
 
 
@@ -17,7 +22,7 @@ const handleSearch = () => {
         return;
     }
     //napravi url za taj grad
-const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=hr`;
    //dohvati vrijeme
 const request = new XMLHttpRequest();
 request.open('GET', url, true);
@@ -28,9 +33,12 @@ request.onload =  () => { //ako ide target.response umjesto request.response ond
     //console.log(e);
     console.log(responseObject);
     const temperature = responseObject.main.temp; 
-    
+    const humidity = responseObject.main.humidity;
+    const pressure = responseObject.main.pressure;
+    const wind = responseObject.wind.speed;
+    const vrijeme = responseObject.weather[0].description;
     const icon = responseObject.weather[0].icon;
-    //DZ
+    //DZ ikonicu riješio, trebam još Clouds, Humidity, Wind, Pressure, Vrijeme
     const urlDio = `http://openweathermap.org/img/w/`; //"http://openweathermap.org/img/w/"
     weatherIconSrc.src = urlDio+icon+'.png';
     IconDisplay.appendChild(weatherIconSrc);
@@ -38,11 +46,22 @@ request.onload =  () => { //ako ide target.response umjesto request.response ond
    
    
 //naći na openWeather u example kodu
-    divRez.innerHTML = `Temperatura je: <b>${temperature}&#8451;</b>`;
+     divRez.innerHTML = `Temperatura je: <b>${temperature}&#8451;</b>`;
+     divRez2.innerHTML = `Vlažnost zraka je: <b>${humidity} %</b>`;
+     divRez3.innerHTML= `Vjetar: <b>${wind} m/s</b>`;
+     divRez4.innerHTML= `Tlak zraka: <b>${pressure} hPa</b>`;
+     divRez5.innerHTML=` Vrijeme: <b>${vrijeme}</b>`;   
+                        
+                        ;
     }
     else if (request.status >= 400 && request.status<=500){
         h1City.innerText = `${city} nije nađen`;
         divRez.innerHTML = '';
+        divRez2.innerHTML='';
+        divRez3.innerHTML='';
+        divRez4.innerHTML='';
+        divRez5.innerHTML='';
+        divRez6.innerHTML='';
     }
     else{
         h1City.innerText = 'Dogodila se pogreška, pokušajte ponovno s unosom!';
